8f1491690fd60785a4460c0096fa62dc2b386004
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-08 10:33:02 -0300

    chore: update libraries and dependencies

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
+import React from "react";
+import { Switch, Route, Redirect } from "react-router";
 
-import AuthOrApp from './authOrApp'
-import Dashboard from '../dashboard/dashboard'
-import BillingCycle from '../billingCycle/billingCycle'
+import AuthOrApp from "./authOrApp";
+import Dashboard from "../dashboard/dashboard";
+import BillingCycle from "../billingCycle/billingCycle";
 
-export default props => (
-    
-    <Router history={hashHistory}>
-        <Route path='/' component={AuthOrAppcd}>
-            <IndexRoute component={Dashboard} />
-            <Route path='billingCycles' component={BillingCycle} />
-        </Route>
-        <Redirect from='*' to='/' />
-    </Router>
-)
+export default (props) => (
+  <div className="content-wrapper">
+    <Switch>
+         <Route path="/" component={AuthOrAppcd}>
+         <Route path="billingCycles" component={BillingCycle} /> </Route>
+         <Redirect from="*" to="/" />
+    </Switch>
+  </div>
+);
